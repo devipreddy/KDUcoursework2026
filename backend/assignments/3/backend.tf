8468bee7a861a22f942c017605f6f62de286aca0
Devi Prasad Reddy P <devi.reddy@kickdrumtech.com>
2026-02-09 17:21:15 +0530

    Cleaning

--- backend/assignments/3/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "my-terraform-state-devi-prasad"
-    key     = "serverless-counter/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
